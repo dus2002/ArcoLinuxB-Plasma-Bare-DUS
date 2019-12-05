5799899d9cd3351ad3a54fb7a02ac2bef23345d0
Daniel <daniurdi46@gmail.com>
2019-12-05 23:51:39 +0100

    Fix the 40* script and then add my installed list of packages

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -185,20 +185,3 @@ echo "################################################################## "
 echo
 
 sudo ./build.sh -v
-
-echo
-echo "################################################################## "
-tput setaf 2;echo "Phase 8 : Moving the iso to out folder";tput sgr0
-echo "################################################################## "
-echo
-
-[ -d $outFolder ] || mkdir $outFolder
-cp $buildFolder/archiso/out/arcolinuxb* $outFolder
-
-echo
-echo "################################################################## "
-tput setaf 2;echo "Phase 9 : Making sure we start with a clean slate next time";tput sgr0
-echo "################################################################## "
-echo
-echo "Deleting the build folder if one exists - takes some time"
-[ -d $buildFolder ] && sudo rm -rf $buildFolder
