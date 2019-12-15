08ca4c0c30fa8c81c72f9e303f38830a4a41a41a
Daniel <daniurdi46@gmail.com>
2019-12-15 15:42:37 +0100

    Add customize_airootfs.sh to the overwritted files

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -74,11 +74,13 @@ rm ../work/archiso/packages.x86_64
 rm ../work/archiso/mkinitcpio.conf
 rm ../work/archiso/pacman.conf
 rm ../work/archiso/pacman.conf.work_dir
+rm ../work/archiso/airootfs/root/customize_airootfs.sh
 echo "Copying the new files"
 cp -f ../archiso/packages.x86_64 ../work/archiso/packages.x86_64
 cp -f ../archiso/mkinitcpio.conf ../work/archiso/mkinitcpio.conf
 cp -f ../archiso/pacman.conf ../work/archiso/pacman.conf
 cp -f ../archiso/pacman.conf.work_dir ../work/archiso/pacman.conf.work_dir
+cp -f ../archiso/airootfs/root/customize_airootfs.sh ../work/archiso/airootfs/root/customize_airootfs.sh
 
 echo "Removing old files/folders from /etc/skel"
 rm -rf ../work/archiso/airootfs/etc/skel/.* 2> /dev/null
