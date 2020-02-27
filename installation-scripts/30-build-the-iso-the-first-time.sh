266ce8a0ad4475186c9cb398330b41b8782fdb31
dus2002 <daniurdi46@gmail.com>
2020-02-27 22:54:45 +0100

    Add DUS name to the distro

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -22,7 +22,7 @@ WorkFolder="$HOME/ArcoLinuxWork
 #Let us change the name"
 #First letter of desktop small
 
-desktop="plasma-bare"
+desktop="plasma-bare-DUS"
 
 #build.sh
 oldname1="iso_name=arcolinux"
